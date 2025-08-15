d66e17c02a7b85fb12b1973ca3a838cb5e2bf9c7
Emanuel <emanuelpeg@yahoo.com.ar>
2025-08-15 01:15:42 -0300

    clase 2

--- objetos/ejemplos/Typescript/promedio/src/Alumno.ts
+++ objetos/ejemplos/Typescript/promedio/src/Alumno.ts
@@ -1,18 +1,58 @@
 export class Alumno{
-    private notas: number[] = [];
+
+    private _notas: number[] = [];
 
     constructor(private nombre: string, private dni: number){}
 
     public promedio() {
-        var acu = 0;
-        for (const nota of this.notas) {
-            acu += nota;
-        }
-        return acu / this.notas.length;
+        var acu = this._notas[0] + this._notas[1] + this._notas[2];
+        return acu / 3;
     }
 
-    public add(nota: number){
-        this.notas.push(nota);
+    public get nota1(): number {
+        return this._notas[0];
     }
 
+    public get nota2(): number {
+        return this._notas[1];
+    }
+
+    public get nota3(): number {
+        return this._notas[2];
+    }
+
+    public set nota1(value : number) {
+        this._notas[0] = value;
+    }
+
+    public set nota2(value : number) {
+        this._notas[1] = value;
+    }
+
+    public set nota3(value : number) {
+        this._notas[2] = value;
+    }
+    
 }
+
+export class Curso{
+
+    private alumnos: Alumno[] = [];
+
+    constructor(private nombre: string){}
+
+    public promedio() {
+        var acu = 0;
+
+        for (const alumno of this.alumnos) {
+            acu += alumno.promedio();
+        }
+        return acu / this.alumnos.length;
+    }
+
+    public add(alumno: Alumno){
+        this.alumnos.push(alumno);
+    }
+}
+
+
--- objetos/ejemplos/Typescript/promedio/src/Main.ts
+++ objetos/ejemplos/Typescript/promedio/src/Main.ts
@@ -2,9 +2,9 @@ import { Alumno } from './Alumno';
 
 function main(): void {
     let unAlumno = new Alumno("Pedro", 3489394);
-    unAlumno.add(44)
-    unAlumno.add(88)
-    unAlumno.add(98)
+    unAlumno.nota1 = 44;
+    unAlumno.nota2 = 88;
+    unAlumno.nota3 = 98;
     console.log(unAlumno.promedio())
 }
 
