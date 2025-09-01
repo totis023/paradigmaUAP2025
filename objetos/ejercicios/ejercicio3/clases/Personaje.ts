5f881905b68c6be342f5edfc02f6bf7975fb9924
Tobias Villarroel <tobivillarroel023@egmail.com>
2025-09-01 18:51:50 -0300

    clase personaje

---
+++ objetos/ejercicios/ejercicio3/clases/Personaje.ts
@@ -0,0 +1,11 @@
+class Personaje {
+        private nombre: string;
+        private vida: string;
+        private poder: number;
+        private ataque: number;
+        private inventario: string[]  = [];
+
+    constructor(
+     
+    ) {}
+}
